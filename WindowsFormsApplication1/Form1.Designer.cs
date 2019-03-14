6fba3bf0b90369b33f66e5ace9c505d012476b2a
Quikit <mr.quikit@gmail.com>
2019-03-14 22:29:12 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox_a = new System.Windows.Forms.TextBox();
+            this.textBox_b = new System.Windows.Forms.TextBox();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.textBox_c = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_bHatch = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 59);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(62, 56);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(31, 20);
+            this.textBox_n.TabIndex = 1;
+            this.textBox_n.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "n =";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Аналитическая ф-я №1",
+            "Аналитическая ф-я №2",
+            "Аналитическая ф-я №3",
+            "Аналитическая ф-я №4",
+            "Аналитическая ф-я №5",
+            "Аналитическая ф-я №6",
+            "Аналитическая ф-я №7",
+            "Аналитическая ф-я №8"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(146, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox_a
+            // 
+            this.textBox_a.Location = new System.Drawing.Point(62, 82);
+            this.textBox_a.Name = "textBox_a";
+            this.textBox_a.Size = new System.Drawing.Size(31, 20);
+            this.textBox_a.TabIndex = 4;
+            this.textBox_a.Text = "0";
+            // 
+            // textBox_b
+            // 
+            this.textBox_b.Location = new System.Drawing.Point(62, 108);
+            this.textBox_b.Name = "textBox_b";
+            this.textBox_b.Size = new System.Drawing.Size(31, 20);
+            this.textBox_b.TabIndex = 5;
+            this.textBox_b.Text = "0";
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.Location = new System.Drawing.Point(62, 160);
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(31, 20);
+            this.textBox_d.TabIndex = 6;
+            this.textBox_d.Text = "0";
+            // 
+            // textBox_c
+            // 
+            this.textBox_c.Location = new System.Drawing.Point(62, 134);
+            this.textBox_c.Name = "textBox_c";
+            this.textBox_c.Size = new System.Drawing.Size(31, 20);
+            this.textBox_c.TabIndex = 6;
+            this.textBox_c.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "a =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "b =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "c =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "d =";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(164, 12);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "Series2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(639, 257);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(99, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "b =";
+            // 
+            // textBox_bHatch
+            // 
+            this.textBox_bHatch.Location = new System.Drawing.Point(127, 108);
+            this.textBox_bHatch.Name = "textBox_bHatch";
+            this.textBox_bHatch.Size = new System.Drawing.Size(31, 20);
+            this.textBox_bHatch.TabIndex = 13;
+            this.textBox_bHatch.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(99, 98);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 281);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_bHatch);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_c);
+            this.Controls.Add(this.textBox_d);
+            this.Controls.Add(this.textBox_b);
+            this.Controls.Add(this.textBox_a);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox_a;
+        private System.Windows.Forms.TextBox textBox_b;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.TextBox textBox_c;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_bHatch;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            double a;
+            double b;
+            double bHatch;
+            double c;
+            double d;
+            double step;
+            // забираем из формы значения
+            n = Convert.ToInt32(textBox_n.Text);
+            a = Convert.ToDouble(textBox_a.Text);
+            b = Convert.ToDouble(textBox_b.Text);
+            c = Convert.ToDouble(textBox_c.Text);
+            d = Convert.ToDouble(textBox_d.Text);
+            bHatch = Convert.ToDouble(textBox_bHatch.Text);
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    
+                    step = (a + b) / n;
+                    for (double x = a; x <= b; x += step)
+                    {
+                        // наполняем график точками
+                        chart1.Series[0].Points.AddXY(x, graphicNumberOne(a, b, x)); 
+                    }
+                    break;
+                case 1:
+                    step = (b + c) / n;
+                    for (double x = a; x <= c + a; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberTwo(a,b,c,x));
+                    }
+                    break;
+                case 2:
+                    step = (a + b) / n;
+                    for (double x = a; x <= b; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberThree(a, b, c, x));
+                    }
+                    break;
+                case 3:
+                    step = ((c+1/a) + c) / n;
+                    for (double x = 0; x <= ((c + 1 / a) + c); x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberFour(a, b, c, x));
+                    }
+                    break;
+                case 4:
+                    step = (a + b) / n;
+                    for (double x = a; x <= b; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberFive(a, b, c, d, x));
+                    }
+                    break;
+                case 5:
+                    step = (a-3*b + a + 3*b) / n;
+                    for (double x = a - 3 * b; x <= a + 3 * b; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberSix(a, b, x));
+                    }
+                    break;
+                case 6:
+                    step = 2*b / n;
+                    for (double x = 0; x <= 2*b; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberSeven(a, b, x));
+                    }
+                    break;
+                case 7:
+                    step = 2 * a / n;
+                    for (double x = 0; x <= 2 * a; x += step)
+                    {
+                        chart1.Series[0].Points.AddXY(x, graphicNumberSix(a, b, x));
+                        chart1.Series[1].Points.AddXY(x, graphicNumberSix(a, bHatch, x));
+                    }
+                    break;
+                default:
+                    
+                    break;
+            }
+            
+        }
+
+        private double graphicNumberOne(double A, double B, double x)
+        {
+            double a;
+            double b;
+            // значение функции
+            double fx = 0;
+                a = A;
+                b = B;
+            
+                if (x <= a)
+                {
+                    fx = 0;                  
+                }
+                if (a <= x && x <= (a + b) / 2)
+                {
+                    fx = (2 * Math.Pow((x - a), 2)) / Math.Pow((b - a), 2);       
+                }
+                if ((a + b) / 2 <= x && x <= b)
+                {
+                   fx = 1 - ((2*Math.Pow((x-b),2))/(Math.Pow((b-a),2)));                  
+                }
+                if (x >= b)
+                {
+                    fx = 1; 
+                }
+                return fx;
+
+        }
+        private double graphicNumberTwo(double A, double B, double C, double x)
+        {
+            double a = A;
+            double b = B;
+            double c = C;
+            double fx = 0;
+                if (a < x && x < b)
+                {
+                    fx = graphicNumberOne(a,b,x);
+                }
+                if (b <= x && x <= b*c)
+                {
+                    fx = 1;
+                }
+                if (c < x && x < c + a)
+                {
+                    fx = 1 - graphicNumberOne(c, c + b - a, x);
+                }
+                if (x >= c + a)
+                {
+                fx = 0;                  
+                }
+            return fx;
+        }
+        private double graphicNumberThree(double A, double B, double C, double x)
+        {
+            double a = A;
+            double b = B;
+            double c = C;
+            double fx = 0;
+            if (x <= a)
+            {
+                fx = 0;
+            }
+            if (a <= x && x <= c)
+            {
+                fx = (x - a) / (c - a);
+            }
+            if (c <= x && x <= b)
+            {
+                fx = (b - x) / (b - c);
+            }
+            if (x >= b)
+            {
+                fx = 0;
+            }
+            return fx;
+        }
+        private double graphicNumberFour(double A, double B, double C, double x)
+        {
+            double a = A;
+            double b = B;
+            double c = C;
+            double fx = 0;
+            if (x < c)
+            {
+                fx = 1;
+            }
+            if (x > c)
+            {
+                fx = Math.Pow((1 + a * (Math.Pow((x - c), b))), -1);
+            }
+            return fx;
+        }
+        private double graphicNumberFive(double A, double B, double C, double D, double x)
+        {
+            double a = A;
+            double b = B;
+            double c = C;
+            double d = D;
+            double fx = 0;
+            if (x <= a)
+            {
+                fx = 0;
+            }
+            if (a < x && x < c)
+            {
+                fx = (x - a) / (c - a);
+            }
+            if (a == x && x == c)
+            {
+                fx = 1;
+            }
+            if (d <= x && x <= b)
+            {
+                fx = (b - x) / (b - d);
+            }
+            if (x >= b)
+            {
+                fx = 0;
+            }
+            return fx;
+        }
+        private double graphicNumberSix(double A, double B, double x)
+        {
+            double a = A;
+            double b = B;
+            double fx = 0;
+            if (x < a - 3 * b)
+            {
+                fx = 0;
+            }
+            if (a-3*b < x && x <a + 3 * b)
+            {
+                fx = Math.Exp(-1 * (Math.Pow((x - a), 2)) / 2 * Math.Pow(b, 2));
+            }
+            if (x > a + 3 * b)
+            {
+                fx = 0;
+            }
+            return fx;
+        }
+        private double graphicNumberSeven(double A, double B, double x)
+        {
+            double a = A;
+            double b = B;
+            return Math.Pow(1 + Math.Exp(-1*(a * (x - b))), -1);
+        }
+            private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
